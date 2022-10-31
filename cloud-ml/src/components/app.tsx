b4ef22382e4090b501e6281f2cf80f90c2fbb300
V31R <kalchenko.vladislaw@yandex.ru>
2022-10-31 15:38:39 +0300

    Переделаны страницы методов

--- cloud-ml/src/components/app.tsx
+++ cloud-ml/src/components/app.tsx
@@ -6,26 +6,13 @@ import Home from "./home";
 import Login from './login';
 import Profile from './profile';
 import Footer from './footer';
-import OperationTemplateComponent from './operationTemplateComponent';
-import RegressionLeft from './regressionLeft';
-import RegressionRight from './regressionRight';
-import DistributionLeft from './distributionLeft';
-import DistributionRight from './distributionRight';
-import DataPreprocessingLeft from './dataPreprocessingLeft';
-import DataPreprocessingRight from './dataPreprocessingRight';
-import ClusterizationLeft from './clusterizationLeft';
-import ClusterizationRight from './clusterizationRight';
+
+import Regression from './regression';
+import Distribution from './distribution';
+import DataPreprocessing from './dataPreprocessing';
+import Clusterization from './clusterization';
 import Correlation from './correlation';
 
-function First(props:any){
-  console.log(props.data);
-  props.setData(props.data+1)
-}
-
-function Second(props:any){
-  console.log(props.data);
-  props.setData(props.data+2)
-}
 
 function App() {
 
@@ -38,10 +25,10 @@ function App() {
           <Route path="/" element={ <Home /> } />
           <Route path="/profile" element={ <Profile /> } />
           <Route path="/login" element={ <Login /> } />
-          <Route path="/regression" element={ <OperationTemplateComponent title ={"Регрессия"} makeRequest={First} left ={<RegressionLeft key = {1}/> } right ={<RegressionRight key = {2}/> } />} />
-          <Route path="/distribution" element={ <OperationTemplateComponent title ={"Распределение данных"} makeRequest={Second} left ={<DistributionLeft key = {1}/> } right ={<DistributionRight key = {2}/> } />} />
-          <Route path="/data-preprocessing" element={ <OperationTemplateComponent title ={"Предварительная обработка данных"} makeRequest={First} left ={<DataPreprocessingLeft key = {1}/> } right ={<DataPreprocessingRight key = {2}/> } />} />
-          <Route path="/clusterization" element={ <OperationTemplateComponent title ={"Кластеризация"} makeRequest={First} left ={<ClusterizationLeft key = {1}/> } right ={<ClusterizationRight key = {2}/> } />} />
+          <Route path="/regression" element={ <Regression />} />
+          <Route path="/distribution" element={ <Distribution /> } />
+          <Route path="/data-preprocessing" element={ <DataPreprocessing />} />
+          <Route path="/clusterization" element={ <Clusterization/>} />
           <Route path="/correlation" element={ <Correlation /> }/>
         </Routes>
         <Footer />
---
+++ cloud-ml/src/components/regression.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../css/operationTemplateComponent.css';
+import '../css/regression.css';
+import MethodPanel from './methodPanel';
+import Header from "./header";
+
+
+function Regression() {
+    const [selectedFile, setSelectedFile] = React.useState<null | any>(null);
+
+    const handleSubmit=(event: any) =>{
+        console.log(selectedFile)
+    }
+
+    const handleFileSelect = (event: any) => {
+        if (event.target.files[0] !== undefined) {
+            setSelectedFile(event.target.files[0]);
+            console.log(event.target.files[0], event.target.files[0].name);
+        } else {
+            setSelectedFile(null);
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <Header />
+            <main className="container">
+                <div className="template-section">
+                    <div>
+                        <h2>Регрессия</h2>
+                    </div>
+                </div>
+                <section className="functions-list row mt-2 mb-3">
+                    <div className="col-md-12 col-lg-6 mt-1">
+                        <div className="function">
+                            <div className='template-title mb-1'>
+                                Параметры
+                            </div>
+                            <MethodPanel onChange={handleFileSelect} onSend={handleSubmit} />
+                        </div>
+                    </div>
+                    <div className="col-md-12 col-lg-6 mt-1">
+                        <div className="function">
+                            <div className='template-title mb-1'>
+                                Результат
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default Regression;
